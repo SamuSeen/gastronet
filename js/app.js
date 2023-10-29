ff0c18abbd3e7a8c346d405f931970376858e8f3
SamuSeen <elooykinge@gmail.com>
2023-10-29 15:28:28 +0100

    fixed service worker

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-if (' serviceWorker' in navigator){
-    navigator.serviceWorker.register('/service-worker.js')
+if (typeof navigator.serviceWorker !== 'undefined') {
+    navigator.serviceWorker.register('sw.js')
         .then(() => console.log('service worker registered'))
         .catch(() => console.log('service worker no registered'))
 }
