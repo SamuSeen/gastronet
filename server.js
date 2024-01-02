8565084b7835fb826c65dc5699ff1ac631dcb498
SamuSeen <elooykinge@gmail.com>
2024-01-02 12:41:54 +0100

    Added phone number as uid

--- server.js
+++ server.js
@@ -46,9 +46,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-
-app.post('/add-subscription', (request, response) => {
-    const { subscription, vapidPublicKey } = req.body;
+app.post("/add-subscription", (request, response) => {
+    const { uid, subscription, vapidPublicKey } = request.body;
 
     // Check if the provided VAPID key matches the expected key
     if (vapidPublicKey !== "BHKnEUIn2lpOowyM4DG9qv96Cxz-jaNHxmpxTw4XowvXxU4Wzl4ThSDCyljYeRyyVBWfJmRByMR5UY2UeuPBRV0") {
@@ -57,22 +56,45 @@ app.post('/add-subscription', (request, response) => {
         console.log("Valid VAPID public key")
     }
 
-    // Handle the subscription
-    console.log('/add-subscription');
-    console.log(request.body);
-    response.sendStatus(200).json({ success: true });
+    //store the subscription in the database with the user identifier
+    db.insert({ uid, subscription }, (err, newDoc) => {
+        if (err) {
+        console.error("Error saving subscription:", err);
+        return response.status(500).json({ error: "Internal Server Error." });
+        }
+        console.log("Subscription saved:", newDoc);
+        response.status(200).json({ success: true });
+    });
 });
 
+
+//todo fix up client-side
+//wait, what actually does this do?
 app.post('/remove-subscription', (request, response) => {
     console.log('/remove-subscription');
     console.log(request.body);
     response.sendStatus(200);
 });
 
-app.post('/notify-me', (request, response) => {
-    console.log('/notify-me');
-    console.log(request.body);
-    response.sendStatus(200);
+app.post("/notify-me", (request, response) => {
+    console.log("/notify-me");
+    const { uid } = request.body;
+    const payload = {
+        title: "Notification for Me",
+        body: "This is a personalized notification.",
+    };
+
+    //find subscriptions for the specified user
+    db.find({ uid }, (err, subscriptions) => {
+        if (err) {
+        console.error("Error fetching subscriptions:", err);
+        return response.status(500).json({ error: "Internal Server Error." });
+        }
+
+        //send notifications to the user subscriptions
+        sendNotifications(payload, subscriptions);
+        response.sendStatus(200);
+    });
 });
 
 app.post('/notify-all', (request, response) => {
